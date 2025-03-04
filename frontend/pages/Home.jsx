0105ccf02bba84a71b1b2ae83725ef4768024f16
Nubchulubs12 <nick.kuczka@gmail.com>
2025-03-03 22:44:15 -0600

    got some foundation made

---
+++ frontend/pages/Home.jsx
@@ -0,0 +1,20 @@
+
+function Home() {
+
+    return (
+        <>
+        <div class="banner">
+            <h1>Moral Support</h1>
+            <p>
+                <a href="https://theschlote.github.io/" target="_blank">Blog</a>
+            </p>
+        </div>
+            <div class="intro">
+<h2> Hello my name is Joey Schlote </h2>
+            </div>
+
+        </>
+    );
+    }
+
+export default Home;
